73bc91ce146a629e26c6711316df30dd24a9803c
klangner <klangner@gmail.com>
2017-03-10 19:03:37 +0100

    Added modulation

--- examples/modulation/src/main.rs
+++ examples/modulation/src/main.rs
@@ -3,7 +3,7 @@ extern crate dsp;
 
 use gnuplot::{Figure, Color, Caption};
 use dsp::vectors::{Vector};
-use dsp::signal::{cosine, sample, modulate};
+use dsp::signal::{cosine, sample, modulate, Signal};
 use dsp::freq::{fft};
 
 // Dimension
@@ -13,23 +13,25 @@ static N: usize = 256;
 fn main() {
     // Our testing signal has 4Hz
     let signal = cosine(10./(N as f64), 0.);
-    let ds = Vector::new(sample(&signal, 0.0, N as f64, 1.));
+    let ds = powers(&signal);
     // Our carrier signal has 50Hz
     let carrier = cosine(50./(N as f64), 0.);
-    let cs = Vector::new(sample(&carrier, 0.0, N as f64, 1.));
+    let cs = powers(&carrier);
+    // Modulated signal
     let modulated = modulate(signal, carrier);
-    let ms = Vector::new(sample(&modulated, 0.0, N as f64, 1.));
+    let ms = powers(&modulated);
 
-    plot(&ds, "Source signal");
-    plot(&cs, "Carrier");
-    plot(&ms, "Modulated signal");
+    let idx: Vec<usize> = (0..N/2).collect();
+    let mut fg = Figure::new();
+    fg.axes2d()
+        .lines(&idx, ds, &[Color("green"), Caption("Source signal")])
+        .lines(&idx, cs, &[Color("blue"), Caption("Carrier signal")])
+        .lines(&idx, ms, &[Color("red"), Caption("Modulated signal")]);
+    fg.show();
 }
 
-fn plot(xs: &Vector, title: &str) {
-    let spectrum = fft(&xs);
-    let powers: Vec<f64> = spectrum.to_vec().iter().map(|x| x.re).collect();
-    let idx: Vec<usize> = (0..xs.len()).collect();
-    let mut fg = Figure::new();
-    fg.axes2d().lines(&idx, powers, &[Color("red"), Caption(title)]);
-    fg.show();
+fn powers(signal: &Signal) -> Vec<f64> {
+    let ds = Vector::new(sample(&signal, 0.0, N as f64, 1.));
+    let spectrum = fft(&ds);
+    spectrum.to_vec().iter().map(|x| x.re).collect()
 }
