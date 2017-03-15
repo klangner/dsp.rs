3623d48b850424c37a160c0cd80ca4676a6fa439
Krzysztof Langner <klangner@gmail.com>
2017-03-15 09:06:51 +0100

    extended modulation example

--- examples/modulation/src/main.rs
+++ examples/modulation/src/main.rs
@@ -11,7 +11,7 @@ const N: usize = 4096;
 
 fn main() {
     // Our testing signal has 1Hz
-    let signal = cosine(1./(N as f64), 0.);
+    let signal = cosine(2./(N as f64), 0.);
     let ds = Vector::new(sample(&signal, (0..N).map(|x| x as f64).collect()));
     // Our carrier signal has 20Hz
     let carrier = cosine(20./(N as f64), 0.);
@@ -22,6 +22,8 @@ fn main() {
 
     time_plot(&ms);
     freq_plot(&ds, &cs, &ms);
+
+    demodulated_plot(&ds, &cs, &ms);
 }
 
 fn time_plot(vector: &Vector) {
@@ -50,3 +52,23 @@ fn powers(v: &Vector) -> Vec<f64> {
     let spectrum = ft.process(&v);
     spectrum.to_vec().iter().map(|x| x.re).collect()
 }
+
+fn demodulated_plot(source: &Vector, carrier: &Vector, modulated: &Vector) {
+    let mut ft = FourierTransform::forward(N, N);
+    let mut fti = FourierTransform::inverse(N, N);
+    let demodulated = modulated.multiply(carrier);
+    // Convert to freq domain to apply filter
+    let ps = ft.process(&demodulated);
+    let ds = fti.process(&ps);
+    let xs: Vec<f64> = source.to_vec().iter().map(|x| x.re).collect();
+    let xs2: Vec<f64> = ds.to_vec().iter().map(|x| x.re/(N as f64)).collect();
+    let idx: Vec<usize> = (0..N).collect();
+    let mut fg = Figure::new();
+    fg.set_terminal("x11 size 800, 400","");
+    fg.axes2d()
+        .lines(&idx, xs, &[Color("green"), Caption("Source signal")])
+        .lines(&idx, xs2, &[Color("red"), Caption("Demodulated signal")]);
+    fg.show();
+
+}
+
