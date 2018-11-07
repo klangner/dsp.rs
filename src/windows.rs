eb96c6956790c69676086ffba09fac60b98ebe7d
Gianmarco Garrisi <gianmarcogarrisi@tutanota.com>
2018-11-07 12:38:32 +0100

    Implement the Weltch window. Considering that Windows are mostly multiplied, store them directly as vectors

--- src/windows.rs
+++ src/windows.rs
@@ -5,73 +5,56 @@ use vectors::{Vector, VectorImpl};
 
 /// A window function. Can be applied to a signal
 #[derive(Clone)]
-pub struct Window<T>
-where
-    T: Fn(f64) -> Complex<f64>,
-{
-    gen: T,
-    len: usize,
+pub struct Window {
+    samples: Vec<f64>,
 }
 
-impl<T> Window<T>
-where 
-      T: Fn(f64) -> Complex<f64>,
-{
-    pub fn into_vec(self) -> Vector {
-        (0..self.len).
-            map(|x| (self.gen)(x as f64))
+impl Window {
+    pub fn into_vector(self) -> Vector {
+        self.samples
+            .into_iter()
+            .map(|x| Complex::new(x, 0.0))
             .collect()
     }
 }
 
 /// Compute a simple rectangular window, a.k.a. __boxcar__ or __Dirichlet__ window
-pub fn rectangular(frame_length: usize) -> Window<impl Fn(f64) -> Complex<f64>> {
+pub fn rectangular(frame_length: usize) -> Window {
     Window {
-        gen: move |x| {
-            if x >= 0. && x < frame_length as f64 {
-                Complex::new(1.0, 0.0)
-            } else {
-                Complex::new(0.0, 0.0)
-            }
-        },
-        len: frame_length,
+        samples: vec![1.0; frame_length],
     }
 }
 
-pub fn triangular(frame_length: usize) -> Window<impl Fn(f64) -> Complex<f64>> {
+pub fn triangular(frame_length: usize) -> Window {
     Window {
-        gen: move |x| {
-            if x >= 0. && x < frame_length as f64 {
-                Complex::new(
-                    1.0 - ((x - (frame_length - 1) as f64 / 2.0)
-                        / ((frame_length - 1) as f64 / 2.0))
-                        .abs(),
-                    0.0,
-                )
-            } else {
-                Complex::new(0.0, 0.0)
-            }
-        },
-        len: frame_length,
+        samples: (0..frame_length)
+            .map(|x| {
+                1.0 - ((x as f64 - (frame_length - 1) as f64 / 2.0)
+                    / ((frame_length - 1) as f64 / 2.0))
+                    .abs()
+            }).collect(),
+    }
+}
+
+pub fn welch(frame_length: usize) -> Window {
+    Window {
+        samples: (0..frame_length)
+            .map(|x| {
+                1.0 - ((x as f64 - (frame_length - 1) as f64 / 2.0)
+                    / ((frame_length - 1) as f64 / 2.0))
+                    .powi(2)
+            }).collect(),
     }
 }
 
 /// Compute a hamming window of the given size
-pub fn hamming(frame_length: usize) -> Window<impl Fn(f64) -> Complex<f64>> {
+pub fn hamming(frame_length: usize) -> Window {
     use std::f64::consts::PI;
     let a0 = 25.0 / 46.0;
 
     Window {
-        gen: move |x| {
-            if x >= 0.0 && x < frame_length as f64 {
-                Complex::new(
-                    a0 - (1.0 - a0) * ((2.0 * PI * x as f64 / (frame_length - 1) as f64).cos()),
-                    0.0,
-                )
-            } else {
-                Complex::new(0.0, 0.0)
-            }
-        },
-        len: frame_length,
+        samples: (0..frame_length)
+            .map(|x| a0 - (1.0 - a0) * ((2.0 * PI * x as f64 / (frame_length - 1) as f64).cos()))
+            .collect(),
     }
 }
