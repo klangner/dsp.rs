f6dc26cfd37cf08f590e1ee6b7483090aef99b0f
Gianmarco Garrisi <gianmarcogarrisi@tutanota.com>
2018-11-17 23:42:05 +0100

    More window functions

--- src/windows.rs
+++ src/windows.rs
@@ -1,8 +1,8 @@
 //! Standard Windows functions
 
 use num_complex::Complex;
-use vectors::{Vector, VectorImpl};
 use signals::Signal;
+use vectors::{Vector, VectorImpl};
 
 /// A window function. Can be applied to a signal
 #[derive(Clone)]
@@ -21,7 +21,8 @@ impl Window {
 
     /// returns a `Vector` without comsuming this window.
     pub fn to_vec(&self) -> Vector {
-        self.samples.iter()
+        self.samples
+            .iter()
             .map(|x| Complex::new(x.clone(), 0.0))
             .collect()
     }
@@ -29,7 +30,10 @@ impl Window {
     /// apply this window to the given signal
     pub fn apply(&self, signal: Signal) -> Signal {
         assert_eq!(self.samples.len(), signal.len());
-        Signal::from_samples(signal.to_vec().multiply(&(self.to_vec())), signal.sample_rate()) 
+        Signal::from_samples(
+            signal.to_vec().multiply(&(self.to_vec())),
+            signal.sample_rate(),
+        )
     }
 }
 
@@ -52,7 +56,6 @@ pub fn triangular(frame_length: usize) -> Window {
     }
 }
 
-
 /// Create the Welch window
 pub fn welch(frame_length: usize) -> Window {
     Window {
@@ -64,15 +67,45 @@ pub fn welch(frame_length: usize) -> Window {
             }).collect(),
     }
 }
+use std::f64::consts::PI;
+
+pub fn sine(frame_length: usize) -> Window {
+    Window {
+        samples: (0..frame_length)
+            .map(|x| (PI * x as f64 / (frame_length - 1) as f64).sin())
+            .collect(),
+    }
+}
+
+pub fn hann(frame_length: usize) -> Window {
+    Window {
+        samples: (0..frame_length)
+            .map(|x| 0.5 * (1.0 - (2.0 * PI * x as f64 / (frame_length - 1) as f64).cos()))
+            .collect(),
+    }
+}
 
 /// Compute a hamming window of the given size
 pub fn hamming(frame_length: usize) -> Window {
-    use std::f64::consts::PI;
     let a0 = 25.0 / 46.0;
 
     Window {
         samples: (0..frame_length)
             .map(|x| a0 - (1.0 - a0) * ((2.0 * PI * x as f64 / (frame_length - 1) as f64).cos()))
             .collect(),
+    }
+}
+
+pub fn blackman(frame_length: usize) -> Window {
+    let a0 = 7938.0 / 18608.0;
+    let a1 = 9240.0 / 18608.0;
+    let a2 = 1430.0 / 18608.0;
+
+    Window {
+        samples: (0..frame_length)
+            .map(|x| {
+                a0 - a1 * (2.0 * PI * x as f64 / (frame_length - 1) as f64).cos()
+                    + a2 * (4.0 * PI * x as f64 / (frame_length - 1) as f64).cos()
+            }).collect(),
     }
 }
