c06d01bd7ed5214f46a6e862bb6e01f24970efd9
Krzysztof Langner <klangner@gmail.com>
2018-11-18 14:14:22 +0100

    Use new, but closure requires Boxes

--- src/generators.rs
+++ src/generators.rs
@@ -13,6 +13,12 @@ pub struct SignalGen {
 }
 
 impl SignalGen {
+
+    /// Create a new generator from provided function
+    pub fn new(f: Box<Fn(f64) -> Complex64>) -> SignalGen {
+        SignalGen{gen: f}
+    }
+
     /// Generate signal at given points
     pub fn generate(&self, points: Vec<f64>) -> Signal {
         let data = points.iter().map(|&i| (self.gen)(i)).collect();
@@ -25,58 +31,59 @@ impl SignalGen {
 /// x[n] = 1 if n == 0
 /// x[n] = 0 if n > 0
 pub fn impulse() -> SignalGen {
-    SignalGen { gen: Box::new(|i| if i == 0. {Complex::new(1., 0.)} else {Complex::new(0., 0.)}) }
+    let sig: Fn(f64) -> Complex64 =
+    SignalGen::new(Box::new(|i| if i == 0. {Complex::new(1., 0.)} else {Complex::new(0., 0.)}))
 }
 
 /// Step signal
 /// x[n] = 1 if n >= 0
 /// x[n] = 0 if n < 0
 pub fn step() -> SignalGen {
-    SignalGen { gen: Box::new(|i| if i >= 0. {Complex::new(1., 0.)} else {Complex::new(0., 0.)}) }
+    SignalGen::new(Box::new(|i| if i >= 0. {Complex::new(1., 0.)} else {Complex::new(0., 0.)}) )
 }
 
 /// Complex sinusoidal signal
 pub fn complex(freq: f64, offset: f64) -> SignalGen {
     let w = 2.0*PI*freq;
-    SignalGen { gen: Box::new(move |i| Complex::new(0., w*(i + offset/2.)).exp()) }
+    SignalGen::new(Box::new(move |i| Complex::new(0., w*(i + offset/2.)).exp()) )
 }
 
 
 /// Real value sine signal
 pub fn sine(freq: f64, offset: f64) -> SignalGen {
     let w = 2.0*PI*freq;
-    SignalGen { gen: Box::new(move |i| Complex::new(f64::sin(w*(i + offset/2.)), 0.)) }
+    SignalGen::new(Box::new(move |i| Complex::new(f64::sin(w*(i + offset/2.)), 0.)) )
 }
 
 
 /// Real value cosine signal
 pub fn cosine(freq: f64, offset: f64) -> SignalGen {
     let w = 2.0*PI*freq;
-    SignalGen { gen: Box::new(move |i| Complex::new(f64::cos(w*(i + offset/2.)), 0.)) }
+    SignalGen::new(Box::new(move |i| Complex::new(f64::cos(w*(i + offset/2.)), 0.)))
 }
 
 
 /// Real value periodic triangle signal (with period of 1 second).
 pub fn triangle(freq: f64) -> SignalGen {
     let w = 2.0*freq;
-    SignalGen { gen: Box::new(move |i| Complex::new((w*(i+0.5)) % 2. - 1., 0.)) }
+    SignalGen::new(Box::new(move |i| Complex::new((w*(i+0.5)) % 2. - 1., 0.)))
 }
 
 /// Real value periodic square signal (with period of 1 second).
 pub fn square(freq: f64) -> SignalGen {
     let w = freq;
-    SignalGen { gen: Box::new(move |i| {
+    SignalGen::new(Box::new(move |i| {
         let a = w*i % 1.;
         let b = if a < -0.5 || (a > 0.0 && a < 0.5) {1.0} else  {-1.0};
         Complex::new(b, 0.)
-    })}
+    }))
 }
 
 
 /// A chirp is a signal in which frequency increases with time.
 pub fn chirp(start_freq: f64, end_freq: f64, time: f64) -> SignalGen {
     let slope = (end_freq - start_freq)/time;
-    SignalGen { gen: Box::new(move |i| {
+    SignalGen::new(Box::new(move |i| {
         if i < 0. || i > time {
             Complex::new(0., 0.)
         } else {
@@ -84,21 +91,16 @@ pub fn chirp(start_freq: f64, end_freq: f64, time: f64) -> SignalGen {
             let w = 2.0*PI*f*i;
             Complex::new(0., w).exp()
         }
-    })}
+    }))
 }
 
 /// A real noise (without imaginary part)
 pub fn noise(std: f64) -> SignalGen {
     let normal = Normal::new(0.0, std);
-    SignalGen { gen: Box::new(move |_| {
+    SignalGen::new(Box::new(move |_| {
         let mut rng = rand::thread_rng();
         Complex::new(normal.ind_sample(&mut rng), 0.0)
-    })}
-}
-
-/// A arbitrary signals generator
-pub fn arbitrary(f: Box<Fn(f64) -> Complex64>) -> SignalGen {
-    SignalGen{gen: f}
+    }))
 }
 
 
