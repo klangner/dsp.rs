605533534b83fa6d1345492c52fa46ff598bcd0a
Gianmarco Garrisi <gianmarcogarrisi@tutanota.com>
2018-12-02 16:37:07 +0100

    Implement apply_with_center() to apply windows to signal with arbitrary lengths

--- src/windows.rs
+++ src/windows.rs
@@ -11,7 +11,12 @@ pub struct Window {
 }
 
 impl Window {
-    /// returns a `Vector` comsuming this window.
+    /// Returns the length of the window, in frames
+    pub fn len(&self) -> usize {
+        self.samples.len()
+    }
+    
+    /// Returns a `Vector` comsuming this window.
     pub fn into_vector(self) -> Vector {
         self.samples
             .into_iter()
@@ -19,7 +24,7 @@ impl Window {
             .collect()
     }
 
-    /// returns a `Vector` without comsuming this window.
+    /// Returns a `Vector` without comsuming this window.
     pub fn to_vec(&self) -> Vector {
         self.samples
             .iter()
@@ -27,13 +32,63 @@ impl Window {
             .collect()
     }
 
-    /// apply this window to the given signal
-    pub fn apply(&self, signal: Signal) -> Signal {
-        assert_eq!(self.samples.len(), signal.len());
+    /// Apply this window to the given signal
+    ///
+    /// # Panics
+    ///
+    /// Panics if the signal and the window have different lengths.
+    ///
+    /// # Returns
+    ///
+    /// A new `Signal` obtained windowing the original.
+    pub fn apply(&self, signal: &Signal) -> Signal {
+        assert_eq!(self.samples.len(), signal.len(), "Signal and window should have the same length");
         Signal::from_samples(
             signal.to_vec().multiply(&(self.to_vec())),
             signal.sample_rate(),
         )
+    }
+
+    /// Apply this window to the given signal centering the window in the
+    /// position specified. Works even when window and signal have different lengths
+    pub fn apply_with_center(&self, signal: &Signal, center: isize) -> Signal {
+        // If the window is applied such that it does not intersect the signal,
+        // or at least one between the signal and the windows has zero length,
+        // we return an empty signal immediately
+        let maxr = center+(self.len()/2) as isize;
+        if maxr < 0  || self.len() == 0 || signal.len() == 0 {
+            return Signal::from_samples(vec![], signal.sample_rate());
+        }
+        
+        // Compute the length of the resulting signal. It is the minimum between
+        // the length of the source signal and the right end of the translated window.
+        // Also compute the right end of the portion of window to consider in the multiplication
+        let mut rs = maxr as usize;
+        let mut rw = self.len();
+        if rs > signal.len() {
+            rw = self.len() - (rs - signal.len());
+            rs = signal.len();
+        }
+        let samples = Vector::with_capacity(rs);
+        // Compute the left end of the singal and of the portion of window to consider
+        let mut ls = center-(self.len()/2) as isize;
+        let mut lw;
+        if ls > 0 {
+            lw = 0;
+            // If the signal to consider starts after the 0, fill the first part of the result
+            // with zeros
+            for _ in 0..ls {
+                samples.push(Complex::new(0.0, 0.0));
+            }
+        } else {
+            lw = -ls;
+            ls = 0;
+        }
+        let (ls, lw) = (ls as usize, lw as usize);
+
+        // FIXME: using a good linalg crate it should be possible to use less allocations here
+        samples.append(&mut signal.to_vec()[ls..rs].multiply(& self.to_vec()[lw..rw].to_vec()));
+        Signal::from_samples(samples, signal.sample_rate())
     }
 }
 
