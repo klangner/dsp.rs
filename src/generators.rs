136fe9fd1c942ff2be50b97bb569a39074304620
klangner <klangner@gmail.com>
2019-07-10 12:46:30 +0200

    Fixed #28 Chirp

--- src/generators.rs
+++ src/generators.rs
@@ -254,11 +254,13 @@ impl SignalGen for NoiseGen {
 
 
 /// A chirp is a signal in which frequency increases with time.
+/// Based on:
+/// https://en.wikipedia.org/wiki/Chirp#Linear
 pub struct ChirpGen {
     current_sample: f32, 
     start_freq: f32,
     end_freq: f32,
-    sweep_samples: f32,
+    sweep_time: f32,
     sample_rate: f32,
 }
 
@@ -269,22 +271,20 @@ impl ChirpGen {
     ///   * length - in seconds
     ///   * sample_rate - How many samples per second
     pub fn new(start_freq: f32, end_freq: f32, sweep_time: f32, sample_rate: usize) -> ChirpGen {
-        let sweep_samples = sweep_time * (sample_rate as f32);
-        ChirpGen { current_sample : 0.0, start_freq, end_freq, sweep_samples, sample_rate: sample_rate as f32}
+        ChirpGen { current_sample : 0.0, start_freq, end_freq, sweep_time, sample_rate: sample_rate as f32}
     }
 }
 
 impl SignalGen for ChirpGen {
 
     fn next(&mut self) -> f32 {
-        if self.current_sample > self.sweep_samples {
+        let t = self.current_sample / self.sample_rate;
+        self.current_sample += 1.0;
+        if t > self.sweep_time {
             0.0
         } else {
-            let slope = (self.end_freq - self.start_freq) / (self.sweep_samples);
-            let sweep_pos = self.current_sample % self.sweep_samples;
-            let freq = slope * sweep_pos + self.start_freq;
-            let w = 2.0 * PI * freq * self.current_sample / self.sample_rate;
-            self.current_sample += 1.0;
+            let c = (self.end_freq - self.start_freq) / (self.sweep_time);
+            let w = 2.0 * PI * (c/2.0*t.powi(2) + self.start_freq*t);
             f32::sin(w)
         }
     }
