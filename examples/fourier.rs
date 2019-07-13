15ad88d4f8a19749a2d851b792e3fbc744bf8379
klangner <klangner@gmail.com>
2019-07-13 19:29:50 +0200

    Buffer converters as a nodes

--- examples/fourier.rs
+++ examples/fourier.rs
@@ -1,24 +1,41 @@
 use gnuplot::{Figure, Color};
 use num_complex::Complex32;
-use dsp::generators::{SignalGen, SineGen};
+
+use dsp::{SourceNode, ProcessingNode, RealToComplexNode, ComplexToRealNode};
+use dsp::generators::{SineGen, GenNode};
 use dsp::fft::{ForwardFFT, InverseFFT};
+
+static SAMPLE_FREQ: f32 = 256.0;
+static DATA_LEN: usize = SAMPLE_FREQ as usize;
 
 
 fn main() {
-    let sample_rate = 256;
-    let mut gen = SineGen::new(4.0, sample_rate as f32);
-    let mut input = (0..sample_rate).map(|_| Complex32::new(gen.next(), 0.0)).collect();
+    
+    // Nodes
+    let mut gen = GenNode::new(Box::new(SineGen::new(4.0, SAMPLE_FREQ)));
+    let mut rtc = RealToComplexNode::new();
+    let mut ctr = ComplexToRealNode::new();
+    let mut fft = ForwardFFT::new(DATA_LEN);
+    let mut ifft = InverseFFT::new(DATA_LEN);
 
-    let mut output = vec![Complex32::new(0.0, 0.0); sample_rate];
-    let mut output2 = vec![Complex32::new(0.0, 0.0); sample_rate];
-    let mut fft = ForwardFFT::new(sample_rate);
-    let mut ifft = InverseFFT::new(sample_rate);
-    fft.process(&mut input, &mut output);
-    ifft.process(&mut output, &mut output2);
+    // Buffers
+    let mut input = vec![0.0; DATA_LEN];
+    let mut input_complex = vec![Complex32::new(0.0, 0.0); DATA_LEN];
+    let mut spectrum = vec![Complex32::new(0.0, 0.0); DATA_LEN];
+    let mut output = vec![Complex32::new(0.0, 0.0); DATA_LEN];
+    let mut output_real = vec![0.0; DATA_LEN];
 
-    let idx: Vec<usize> = (0..sample_rate).collect();
-    let powers: Vec<f32> = output.iter().map(|&x| x.norm()).collect();
+    // Process graph
+    gen.next_batch(&mut input);
+    rtc.process(&input, &mut input_complex);
+    fft.process(&mut input_complex, &mut spectrum);
+    ifft.process(&mut spectrum, &mut output);
+    ctr.process(&output, &mut output_real);
+
+    // Visualize
+    let idx: Vec<usize> = (0..DATA_LEN).collect();
     let mut fg = Figure::new();
-    fg.axes2d().lines(&idx, &powers, &[Color("blue")]);
+    fg.axes2d().lines(&idx, &input, &[Color("red")]);
+    fg.axes2d().lines(&idx, &output_real, &[Color("blue")]);
     fg.show();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -19,45 +19,100 @@ pub type RealBuffer = Vec<f32>;
 /// Frequency domain data buffer based on complex numbers
 pub type ComplexBuffer = Vec<Complex32>;
 
-
-// impl RealBuffer {
-//     pub fn to_complex(&self, output: &mut ComplexBuffer) {
-//         let n = usize::min(self.data.len(), output.len());
-//         for i in 0..n {
-//             output[i] = Complex32::new(self.data[i], 0.0);
-//         }
-//     }
-// }
-
-// impl ComplexBuffer {
-//     pub fn to_real(&self, output: &mut RealBuffer) {
-//         let n = usize::min(self.data.len(), output.len());
-//         for i in 0..n {
-//             output[i] = self[i].norm();
-//         }
-//     }
-// }
-
-
-trait SourceNode {
+/// Node which produces signal
+pub trait SourceNode {
     type Buffer;
     
     fn next_batch(&mut self, output: &mut Self::Buffer);
 }
 
-trait ProcessingNode {
+// signal transformation
+pub trait ProcessingNode {
     type InBuffer;
     type OutBuffer;
     
-    fn next(input: &Self::InBuffer, output: &mut Self::OutBuffer);
+    fn process(&mut self, input: &Self::InBuffer, output: &mut Self::OutBuffer);
 }
 
-trait SinkNode {
+// Consume signal
+pub trait SinkNode {
     type Buffer;
     
-    fn next(input: &Self::Buffer);
+    fn consume(&mut self, input: &Self::Buffer);
 }
 
+pub struct RealToComplexNode {}
 
+impl RealToComplexNode {
+    pub fn new() -> RealToComplexNode {
+        RealToComplexNode {}
+    }
+}
 
+impl ProcessingNode for RealToComplexNode {
+    type InBuffer = RealBuffer;
+    type OutBuffer = ComplexBuffer;
+    
+    fn process(&mut self, input: &RealBuffer, output: &mut ComplexBuffer) {
+        let n = usize::min(input.len(), output.len());
+        for i in 0..n {
+            output[i] = Complex32::new(input[i], 0.0);
+        }
+    }
+}
 
+pub struct ComplexToRealNode {}
+
+impl ComplexToRealNode {
+    pub fn new() -> ComplexToRealNode {
+        ComplexToRealNode {}
+    }
+}
+
+impl ProcessingNode for ComplexToRealNode {
+    type InBuffer = ComplexBuffer;
+    type OutBuffer = RealBuffer;
+    
+    fn process(&mut self, input: &ComplexBuffer, output: &mut RealBuffer) {
+        let n = usize::min(input.len(), output.len());
+        for i in 0..n {
+            output[i] = input[i].re;
+        }
+    }
+}
+
+/// ------------------------------------------------------------------------------------------------
+/// Module unit tests
+/// ------------------------------------------------------------------------------------------------
+#[cfg(test)]
+mod tests {
+    use num_complex::Complex32;
+    use super::*;
+
+    #[test]
+    fn test_real_to_complex() {
+        let real = vec![1.0, 2.0, 3.0, 4.0];
+        let mut complex = vec![Complex32::new(0.0, 0.0); 4];
+        let expected = vec![Complex32::new(1.0, 0.0), 
+                            Complex32::new(2.0, 0.0),
+                            Complex32::new(3.0, 0.0),
+                            Complex32::new(4.0, 0.0)];
+        let mut rtc = RealToComplexNode::new();
+        rtc.process(&real, &mut complex);
+        assert_eq!(&complex, &expected);
+    }
+
+    #[test]
+    fn test_complex_to_real() {
+        let complex = vec![Complex32::new(1.0, 0.0), 
+                           Complex32::new(2.0, 0.0),
+                           Complex32::new(3.0, 0.0),
+                           Complex32::new(4.0, 0.0)];
+        let mut real = vec![0.0; 4];
+        let expected = vec![1.0, 2.0, 3.0, 4.0];
+        let mut ctr = ComplexToRealNode::new();
+        ctr.process(&complex, &mut real);
+        assert_eq!(&real, &expected);
+    }
+
+}
