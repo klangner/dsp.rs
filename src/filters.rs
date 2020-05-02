a11852b080e57401e5203a26ad8b99a51c841e93
Mark Kubiak <mkubiak.dev@gmail.com>
2020-05-02 17:15:20 -0400

    Added unit test for biquad filter to verify validity of bilinear transform. Matches behavior of scipy.signal perfectly.

--- src/filters.rs
+++ src/filters.rs
@@ -1,5 +1,4 @@
 /// Basic implementations of common discrete filters
-use std::ops::{Add, AddAssign, Mul};
 use arraydeque::{ArrayDeque, Wrapping};
 use itertools::{izip};
 use crate::{RealBuffer, ProcessingNode};
@@ -24,24 +23,34 @@ impl BiquadNode {
         // Sanity check
         assert_eq!(b.len(), 3);
         assert_eq!(a.len(), 3);
-        assert_ne!(a[0], Default::default()); // a0 of 0 results in divide by 0
+        assert_ne!(a[0], 0.0); // a0 of 0 results in divide by 0
 
         // Initialize sample histories
         let mut x: ArrayDeque<[f32; 3], Wrapping> = ArrayDeque::new();
         let mut y: ArrayDeque<[f32; 3], Wrapping> = ArrayDeque::new();
         for _ in 0..3 {
-            x.push_front(Default::default());
-            y.push_front(Default::default());
+            x.push_front(0.0);
+            y.push_front(0.0);
         }
 
-        // Clone elements from passed in slice
-        let mut b_arr: [f32; 3] = Default::default();
-        let mut a_arr: [f32; 3] = Default::default();
+        // Clone the b coefficients from passed in slice
+        let mut b_arr: [f32; 3] = [0.0; 3];
         b_arr.clone_from_slice(b);
-        a_arr.clone_from_slice(a);
+
+        // Clone the a coefficients, inverting a[1..] by
+        // the definition of an IIR filter
+        let mut neg_a_arr: [f32; 3] = [0.0; 3];
+        neg_a_arr[0] = a[0];
+        for i in 1..3 {
+            neg_a_arr[i] = -a[i];
+        }
 
         // New filter with x/y initalized to same length as a/b
-        BiquadNode {x, y, b: b_arr, a: a_arr, output: Default::default()}
+        BiquadNode {
+            x, y,
+            b: b_arr, a: neg_a_arr,
+            output: Default::default()
+        }
     }
 
     /// Process one sample of the input signal and returns one sample of the
@@ -54,7 +63,7 @@ impl BiquadNode {
         self.y.pop_back();
 
         // Compute the filter result
-        let mut sum = Default::default();
+        let mut sum = 0.0;
         for (xi, bi) in izip!(self.x.iter(), self.b.iter()) {
             sum += *xi * *bi;
         }
@@ -88,5 +97,50 @@ impl ProcessingNode for BiquadNode {
 
         // Return a ref to the now-valid output buffer
         &self.output
+    }
+}
+
+
+/// ------------------------------------------------------------------------------------------------
+/// Module unit tests
+/// ------------------------------------------------------------------------------------------------
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_approx_eq::assert_approx_eq;
+
+    #[test]
+    fn test_biquad_bilinear_rc() {
+
+        // Test our biquad by using the bilinear transform to create
+        // a digital filter with similar response to a basic analog RC
+        // filter with constants T=0.1s RC=1.
+        // Generate and compare their step responses over 5 seconds.
+        let rc = 1.0;
+        let t_samp = 0.1;
+
+        // Analog step response evaluated every T seconds.
+        let mut analog_response = Vec::new();
+        for i in 0..50 {
+            let t = (i as f32) * t_samp;
+            analog_response.push(1.0 - f32::exp(-t / rc));
+        }
+
+        // Compute and run the equivalent digital filter.
+        // Bilinear transform of RC filter:
+        // https://en.wikipedia.org/wiki/Bilinear_transform#Example
+        let b = [1.0, 1.0, 0.0];
+        let a = [1.0+(2.0*rc/t_samp), 1.0-(2.0*rc/t_samp), 0.0];
+        println!("a: {:#?}", a);
+        let mut biquad_rc = BiquadNode::new(&b, &a);
+        let dig_unit_step = vec![1.0; 50];
+        let digital_response = biquad_rc.process(&dig_unit_step);
+        
+        // Compare the filter unit step responses. Since there
+        // is some difference between the initial state of the
+        // filters, use a less aggressive 5% threshold
+        for i in 0..50 {
+            assert_approx_eq!(analog_response[i], digital_response[i], 0.05);
+        }
     }
 }
