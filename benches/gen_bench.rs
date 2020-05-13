987a13451f813535b6f23dd4c0f1cdd7ec5453e6
klangner <klangner@gmail.com>
2020-05-13 09:22:15 +0200

    fixed benchmarks

--- benches/gen_bench.rs
+++ benches/gen_bench.rs
@@ -7,24 +7,19 @@ extern crate criterion;
 use criterion::Criterion;
 use criterion::black_box;
 
-use dsp::generators::{SignalGen, SineGen};
+use dsp::generators::*;
 
 
-static SAMPLE_FREQ: f32 = 1_000.0;
-
-
-fn generate_sine(n : usize) -> f32 {
-    let gen = SineGen::new(220.0);
-    let mut s = 0.0;
-    for i in 0..(n*SAMPLE_FREQ as usize) {
-        s += gen.sample(i as f32 / SAMPLE_FREQ);
-    }
-    s
+fn generate_sine(n : usize) -> Vec<f32> {
+    let signal = sine(n, 50.0, n);
+    signal
 }
 
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("Sine 1M samples", |b| b.iter(|| generate_sine(black_box(1_000))));
+    c.bench_function("Sine generator", |b| {
+        b.iter(|| generate_sine(black_box(1_000)))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
