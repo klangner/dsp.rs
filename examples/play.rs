b92f0f4c8b6330703b807c6147164345ffa2836f
klangner <klangner@gmail.com>
2022-08-18 10:44:41 +0200

    workin play example

--- examples/play.rs
+++ examples/play.rs
@@ -1,10 +1,38 @@
 
-use dsp::node::audio::AudioSink;
+use dsp::node::generator::Sine;
+use dsp::runtime::node::SourceNode;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+
 
 const SAMPLE_RATE: u32 = 44100;
+const BUFFER_SIZE: u32 = 1024;
 
 
 fn main() {
-    let _audio_sink = AudioSink::new(SAMPLE_RATE);
-    std::thread::sleep(std::time::Duration::from_millis(1000));
+    let mut generator = Sine::new(440., SAMPLE_RATE as usize);
+
+    // Init output device
+    let host = cpal::default_host();
+
+    let device = host
+        .default_output_device()
+        .ok_or_else(|| anyhow::Error::msg("Default output device is not available")).unwrap();
+
+    let config = cpal::StreamConfig {
+        channels: 1,
+        sample_rate: cpal::SampleRate(SAMPLE_RATE),
+        buffer_size: cpal::BufferSize::Fixed(BUFFER_SIZE),
+    };
+
+    let stream = device.build_output_stream(
+        &config,
+        move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+            generator.write_buffer(data).unwrap();
+        },
+        move |err| {panic!("cpal stream error {:?}", err);}
+    ).unwrap();
+    
+    stream.play().unwrap();
+    
+    std::thread::sleep(std::time::Duration::from_millis(1_000));
 }
--- src/node/audio/mod.rs
+++ src/node/audio/mod.rs
@@ -3,8 +3,6 @@
 //! This module contains various SourceNodes
 //! 
 pub mod audio_file;
-pub mod audio_sink;
 
 
 pub use audio_file::AudioFileSource;
-pub use audio_sink::AudioSink;
